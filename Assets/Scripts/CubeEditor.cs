c0dd845a36c379b7e05f898e2da6883daebfc5af
Jakub Dockal <jakub.dockal@gmail.com>
2018-10-10 08:38:53 +0200

    position labels

--- Assets/Scripts/CubeEditor.cs
+++ Assets/Scripts/CubeEditor.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 
 [ExecuteInEditMode]
+[SelectionBase]
 public class CubeEditor : MonoBehaviour {
 
 	[Range(1f, 20f)][SerializeField] float gridSize = 10f;
+	TextMesh textMesh;
+	Vector3 snapPos;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		Vector3 snapPos;
+		CubeSnapping();
+
+		LabelWriting();
+	}
+
+	private void LabelWriting()
+	{
+		textMesh = GetComponentInChildren<TextMesh>();
+		textMesh.text = snapPos.x/gridSize + "," + snapPos.z/gridSize;
+	}
+
+	private void CubeSnapping()
+	{
 		snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
 		snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 		transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
