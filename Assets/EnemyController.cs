0588776a8c2336cb9e42942b94d67a3d93a13722
JDHome <jakub.dockal@gmail.com>
2018-10-10 17:15:14 +0200

    coroutine

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -4,20 +4,29 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
 
-	[SerializeField] List<BlockScript> path;
+	[SerializeField] List<Waypoint> path;
 
 	// Use this for initialization
 	void Start()
 	{
-
+		StartCoroutine(FollowPath());
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		foreach (BlockScript block in path)
+
+	}
+
+	IEnumerator FollowPath()
+	{
+		print("Starting patrol.");
+		foreach (Waypoint waypoint in path)
 		{
-			print(block.name);
+			transform.position = waypoint.transform.position;
+			print("I`m currently patroling on coordinates " + waypoint.name);
+			yield return new WaitForSeconds(1f);
 		}
+		print("I arrived at the patroling destination.");
 	}
 }
--- Assets/Waypoint.cs
+++ Assets/Waypoint.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlockScript : MonoBehaviour {
+public class Waypoint : MonoBehaviour {
+
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 		
 	}
 	
