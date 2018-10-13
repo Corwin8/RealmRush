1200109f32c9c3e2ee5cab3cdbc70ee0aeaf4e83
Jakub Dockal <jakub.dockal@gmail.com>
2018-10-13 15:58:28 +0200

    coroutine

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+	[SerializeField] EnemyController enemyPrefabToSpawn;
+	[SerializeField] float secondsBetweenSpawns = 2f; 
+
+	// Use this for initialization
+	void Start ()
+	{
+		StartCoroutine(SpawnEnemies());
+	}
+
+	IEnumerator SpawnEnemies()
+	{
+		while (true) //forever
+		{
+			Instantiate(enemyPrefabToSpawn, new Vector3(0, 0, 0), Quaternion.identity);
+			yield return new WaitForSeconds(secondsBetweenSpawns);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
